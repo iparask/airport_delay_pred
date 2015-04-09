8b34fe1a862e6deb40a50519f5e5382a69a2f217
iparask <ioannis.paraskevakos@rutgers.edu>
2015-04-09 15:18:31 -0400

    Data Files upload

--- python_src/simulation_script.py
+++ python_src/simulation_script.py
@@ -23,23 +23,33 @@ def read_csv_from_dir(path, cols, col_types=None):
     return pd.concat(pieces, ignore_index=True)
 
 if __name__ == '__main__':
+
     # read files
     cols = ['delay', 'month', 'day', 'dow', 'hour', 'distance', 'carrier', 'dest', 'days_from_holiday']
     col_types = {'delay': int, 'month': int, 'day': int, 'dow': int, 'hour': int, 'distance': int, 
 	   'carrier': str, 'dest': str, 'days_from_holiday': int}
-    data_2005 = read_csv_from_dir('../../ord_2005_1', cols, col_types)
-    data_2006 = read_csv_from_dir('../../ord_2006_1', cols, col_types)
-    data_2007 = read_csv_from_dir('../../ord_2007_1', cols, col_types)
-    data_2008 = read_csv_from_dir('../../ord_2008_1', cols, col_types)
+    data_2002 = read_csv_from_dir('../data/ord_2002_1', cols, col_types)
+    data_2003 = read_csv_from_dir('../data/ord_2003_1', cols, col_types)
+    data_2004 = read_csv_from_dir('../data/ord_2004_1', cols, col_types)
+    data_2005 = read_csv_from_dir('../data/ord_2005_1', cols, col_types)
+    data_2006 = read_csv_from_dir('../data/ord_2006_1', cols, col_types)
+    data_2007 = read_csv_from_dir('../data/ord_2007_1', cols, col_types)
+    data_2008 = read_csv_from_dir('../data/ord_2008_1', cols, col_types)
 
     # Create training set and test set
     cols = ['month', 'day', 'dow', 'hour', 'distance', 'days_from_holiday']
-    train_y = data_2005['delay'] >= 15
+    train_y = data_2002['delay'] >= 15
+    train = data_2003['delay'] >= 15
+    train_y = [train_y]#, train]
+    train = data_2004['delay'] >= 15
+    train_y.append(train)
+    train = data_2005['delay'] >= 15
+    train_y.append(train)
     train = data_2006['delay'] >= 15
-    train_y = [train_y, train]
+    train_y.append(train)
     train = data_2007['delay'] >= 15
     train_y.append(train)
-    train_x = [data_2005[cols],data_2006[cols],data_2007[cols]]
+    train_x = [data_2002[cols],data_2003[cols],data_2004[cols],data_2005[cols],data_2006[cols],data_2007[cols]]
     test_y = data_2008['delay'] >= 15
     test_x = data_2008[cols]
 
