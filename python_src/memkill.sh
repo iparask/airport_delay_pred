2865871caddd2cd96756866456625cf295b2b7e8
Ali Rostami <rostami@math1.winlab.rutgers.edu>
2015-04-26 18:59:42 -0400

    Changed the username in memkol.sh

--- python_src/memkill.sh
+++ python_src/memkill.sh
@@ -7,12 +7,12 @@ do
 
   if [ `awk '/^Mem/ {printf("%u", 100*$3/$2);}' <(free -m)` -gt $THRESHOLD ]
   then
-    ps aux | grep -i python | awk '{if($1=="iparask") print $2}' | while read -r pid ; do
+    ps aux | grep -i python | awk '{if($1=="rostami") print $2}' | while read -r pid ; do
         #to write the kill "$pid"
         echo "$pid"
         kill -9  "$pid"
     done
   fi
-  sleep 5
+  sleep 2
 
 done
